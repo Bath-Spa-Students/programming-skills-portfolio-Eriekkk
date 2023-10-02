1f827c76d097beee60eebdeb0a97fdcd492559e4
Eriekkk <erielpsalmgalura@gmail.com>
2023-10-02 11:59:15 +0400

    practice 1.py

---
+++ Chapter 1- Getting Started with Python/practice 4.py
@@ -0,0 +1,5 @@
+print("Computing the area of the triangle")
+L = int(input("Enter the lenght of triangle: "))
+W = int(input("Enter the width of triangle: "))
+area = 0.5 * L * W
+print("the area of the triangle is", area)
---
+++ Chapter 1- Getting Started with Python/practice 5.py
@@ -0,0 +1,5 @@
+a = "laptop"
+b = "mouse"
+c = "keyboard"
+d = "monitor"
+print(a,b,c,d)
